ec6a69d4c4f673fdec6cbaa3fcec89e040c489dc
jatinssh <jatin7838066@gmail.com>
2024-08-03 16:43:57 +0530

    updated

--- portfolio-frontend/src/constants/index.js
+++ portfolio-frontend/src/constants/index.js
@@ -3,17 +3,17 @@ import portfoliojatin from "../assets/projects/portfoliojatin.png";
 import SalonProject from "../assets/projects/SalonProject.png";
 // import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React , as well as back-end technologies like Node.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer,  I specialize in creating dynamic websites. My expertise spans front-end technologies like React.js, including proficiency in Framer Motion and Tailwind CSS, as well as back-end technologies such as Express.js, Node.js, and MongoDB. Currently, I am in the final year of my Bachelor's in Computer Science, specializing in Cyber Security with a minor in Blockchain. I thrive in environments that offer challenging and evolving tech stacks, where continuous learning and innovation are at the core of the work. Additionally, I practice DSA (Data Structures and Algorithms) questions daily using Java to continually hone my problem-solving skills.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I practice Data Strctures Questions dauly to improve my logical ability. Outside of coding, I enjoy staying active, exploring new technologies,currently exploring blockchain technology.`;
+export const ABOUT_TEXT = `I am a dedicated and resilient full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js,ExpressJs and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I practice Data Strctures Questions dauly to improve my logical ability. Outside of coding, I enjoy staying active, exploring new technologies,currently exploring blockchain technology.`;
 
 export const EXPERIENCES = [
   {
     year: "June,2024 - Present",
     role: "Full Stack Developer Intern",
     company: "Lagaland",
-    description: `I along with my team member are working on making Salon website,I am working on Backend part using  Node.js,ExpressJs. Implementing RESTful APIs and integrating with MongoDB databases. `,
-    technologies: ["Javascript", "ExpressJs", "mongoDB"],
+    description: `I along with my team member are working on making Salon website,I am working on Backend part using  Node.js,ExpressJs. Implementing RESTful APIs for signin,signup of salon user,updating their salon time on website,and uploading photos of their salons, storing data on MongoDB databases(storing link of image on mongodb,image is uploaded to cloudinary). `,
+    technologies: [ "ExpressJs", "MongoDB"],
   },
   
 ];
